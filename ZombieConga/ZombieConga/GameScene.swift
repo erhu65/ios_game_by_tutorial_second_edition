0cc7d33ecc58a4d20b52ef33f75fe6246b0eb7f4
Peter Huang <peter.huang@twseeds.com>
2015-04-26 17:12:13 +0800

    Chapter 1: Sprites. -- Challenges

--- ZombieConga/ZombieConga/GameScene.swift
+++ ZombieConga/ZombieConga/GameScene.swift
@@ -9,17 +9,24 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    let zombie: SKSpriteNode = SKSpriteNode(imageNamed: "zombie1")
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
         backgroundColor = SKColor.grayColor()
         let background = SKSpriteNode(imageNamed: "background1")
         background.position = CGPoint(x: size.width/2, y: size.height/2)
         background.anchorPoint = CGPoint(x: 0.5, y: 0.5) // default
-        background.zRotation = CGFloat(M_PI) / 8
-        background.zPosition = -1
+       // background.zRotation = CGFloat(M_PI) / 8
+        //background.zPosition = -1
         addChild(background)
         let mySize = background.size
         println("Size: \(mySize)")
+        
+        
+        zombie.position = CGPoint(x: 400, y: 400)
+        addChild(zombie)
+        zombie.setScale(2.0) // SKNode method
         
     }
     
