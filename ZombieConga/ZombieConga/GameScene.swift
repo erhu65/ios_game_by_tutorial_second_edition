9ea5cee59c647ec52189748debfcf8bd2716f2cb
Peter Huang <peter.huang@twseeds.com>
2015-04-26 17:02:32 +0800

    Chapter 1: Sprites -- Displaying a sprite

--- ZombieConga/ZombieConga/GameScene.swift
+++ ZombieConga/ZombieConga/GameScene.swift
@@ -12,6 +12,15 @@ class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
         backgroundColor = SKColor.grayColor()
+        let background = SKSpriteNode(imageNamed: "background1")
+        background.position = CGPoint(x: size.width/2, y: size.height/2)
+        background.anchorPoint = CGPoint(x: 0.5, y: 0.5) // default
+        background.zRotation = CGFloat(M_PI) / 8
+        background.zPosition = -1
+        addChild(background)
+        let mySize = background.size
+        println("Size: \(mySize)")
+        
     }
     
    
